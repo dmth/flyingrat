12114d07801d2af0ae31c9fd2954bc893b546766
Bernhard Reiter <bernhard@intevation.de>
2017-08-15 09:24:47 +0200

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*paths):
 
 setup(
     name='flyingrat',
-    version='0.2.2',
+    version='0.2.3',
     description='Simple mail server for local development',
     long_description=read('README.rst'),
     url='https://github.com/dmth/flyingrat/',
